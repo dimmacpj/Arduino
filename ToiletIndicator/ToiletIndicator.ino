5e270e922c58662c442d772c7b1c72b282001f9d
dimmacpj <dimmacpj3@sina.com>
2019-04-28 03:09:48 -0700

    Toilet_Version_1

--- ToiletIndicator/ToiletIndicator.ino
+++ ToiletIndicator/ToiletIndicator.ino
@@ -1,9 +1,20 @@
+const int LED = 11;
+const int PIR = 3;
+int val = 0;
+int state = 0;
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(LED, OUTPUT);
+  pinMode(PIR, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  val = digitalRead(PIR);
+  if(val == 1){
+    digitalWrite(LED, HIGH);
+  }else{
+    digitalWrite(LED, LOW);
+  }
 }
